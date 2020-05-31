a1e2de9d812f7c889e70d6ab3c122e14a3abc80d
rrokkam <rohithrokkam@gmail.com>
2020-05-31 15:36:42 -0700

    Reorder functions

--- src/non_total.rs
+++ src/non_total.rs
@@ -19,6 +19,19 @@ pub fn binary_entropy(p: f64) -> f64 {
     information_content(p) + information_content(1.0 - p)
 }
 
+fn information_content(p: f64) -> f64 {
+    if !is_valid_probability(p) {
+        panic!("Probability not between 0 or 1");
+    }
+
+    // Follow convention that the information content of a zero-probability event is 0.
+    if p == 0.0 {
+        return 0.0;
+    }
+
+    -p * f64::log2(p)
+}
+
 /// True iff p is in [0, 1].
 ///
 /// # Examples
@@ -40,17 +53,4 @@ pub fn binary_entropy(p: f64) -> f64 {
 ///
 pub fn is_valid_probability(p: f64) -> bool {
     p >= 0.0 && p <= 1.0
-}
-
-fn information_content(p: f64) -> f64 {
-    if !is_valid_probability(p) {
-        panic!("Probability not between 0 or 1");
-    }
-
-    // Follow convention that the information content of a zero-probability event is 0.
-    if p == 0.0 {
-        return 0.0;
-    }
-
-    -p * f64::log2(p)
 }
