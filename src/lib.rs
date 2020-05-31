6b9aa781afb2ad119d3cc229b06e52b123f81f83
rrokkam <rohithrokkam@gmail.com>
2020-05-31 16:11:59 -0700

    Implement Option-based binary entropy function

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+pub mod maybe;
 pub mod non_total;
---
+++ src/maybe.rs
@@ -0,0 +1,58 @@
+/// Returns the binary entropy of a Bernoulli variable with probability p as one of its values.
+///
+/// # Examples
+///
+/// ```
+/// # use probability_ghosts::maybe::binary_entropy;
+/// assert_eq!(binary_entropy(0.5), Some(1.0));
+/// ```
+///
+/// ```
+/// # use probability_ghosts::maybe::binary_entropy;
+/// assert_eq!(binary_entropy(0.0), Some(0.0));
+/// ```
+///
+/// ```
+/// # use probability_ghosts::maybe::binary_entropy;
+/// assert_eq!(binary_entropy(2.0), None);
+/// ```
+///
+pub fn binary_entropy(p: f64) -> Option<f64> {
+    let probability = to_probability(p)?;
+    Some(information_content(p) + information_content(1.0 - p))
+}
+
+fn information_content(p: f64) -> f64 {
+    // Follow convention that the information content of a zero-probability event is 0.
+    if p == 0.0 {
+        return 0.0;
+    }
+
+    -p * f64::log2(p)
+}
+
+/// True iff p is in [0, 1].
+///
+/// # Examples
+///
+/// ```
+/// # use probability_ghosts::maybe::to_probability;
+/// assert_eq!(to_probability(f64::NAN), None);
+/// ```
+///
+/// ```
+/// # use probability_ghosts::maybe::to_probability;
+/// assert_eq!(to_probability(-1.0), None);
+/// ```
+///
+/// ```
+/// # use probability_ghosts::maybe::to_probability;
+/// assert_eq!(to_probability(0.5), Some(0.5));
+/// ```
+///
+pub fn to_probability(p: f64) -> Option<f64> {
+    if p >= 0.0 && p <= 1.0 {
+        return Some(p);
+    }
+    None
+}
